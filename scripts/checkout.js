4193c8c17a8d05dd0e37350cc1effc7d981744f6
Krati <xyz@gmail.com>
2024-07-23 22:37:02 +0530

    learn promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,8 +5,26 @@ import { loadCart } from "../data/cart.js";
 // import '../data/cart-class.js';
 // import "../data/backend-practice.js";
 
-new Promise((resolve) => {
+Promise.all([
+    new Promise((resolve) => {
+        loadProducts(() => {
+            resolve('value1');
+        });
+    }),
+    new Promise((resolve) => {
+        loadCart(()=>{
+            resolve();
+        });
+    }),
+]).then( (values)=>{
+    console.log(values);
+    renderOrderSummary();
+    renderPaymentSummary();
+});
 
+
+
+/*new Promise((resolve) => {
     loadProducts(() => {
         resolve();
     });
